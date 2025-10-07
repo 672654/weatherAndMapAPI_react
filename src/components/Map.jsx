8fc0ccbccaaca0fa27c7ef84e6e9696ef153d0c1
672654 <henrik_krane@hotmail.com>
2025-10-07 21:03:26 +0200

    some css

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,15 +1,29 @@
 import "leaflet/dist/leaflet.css";
+import { useState } from "react";
 import { MapContainer, Marker, TileLayer } from "react-leaflet";
+import "../css/Map.css";
 
 function MapComponent({ lat, lon }) {
+  const mapViews = {
+    dark: "https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png",
+    light: "https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png",
+    satelite: "https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png",
+  };
+
+  const [view, setView] = useState("dark");
+
   return (
     <>
-      <MapContainer
-        center={[lat, lon]}
-        zoom={10}
-        style={{ height: "400px", width: "100%" }}
-      >
-        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      <div className="select-menu">
+        <label>Map Style:</label>
+        <select value={view} onChange={(e) => setView(e.target.value)}>
+          <option value="dark">Dark</option>
+          <option value="light">Light</option>
+          <option value="satelite">Satellite</option>
+        </select>
+      </div>
+      <MapContainer className="map-container" center={[lat, lon]} zoom={10}>
+        <TileLayer url={mapViews[view]} />
         <Marker position={[lat, lon]}></Marker>
       </MapContainer>
     </>
